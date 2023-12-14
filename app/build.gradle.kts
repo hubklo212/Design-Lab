fe73d306ae97d392547e1908fddf02a896e973ce
hubklo212 <klochubert@gmail.com>
2023-12-14 01:38:01 +0100

    version 0.3 now fetches user's location and calls an api request with received parameters (latitude and longitude) to get the sunset time

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.hubklo212.sunsetled"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.hubklo212.sunsetled"
@@ -13,6 +13,7 @@ android {
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
+
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -50,6 +51,15 @@ android {
 }
 
 dependencies {
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
+
+    // FusedLocationProvider
+    implementation("com.google.android.gms:play-services-location:18.0.0")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
--- app/src/main/java/com/hubklo212/sunsetled/MainActivity.kt
+++ app/src/main/java/com/hubklo212/sunsetled/MainActivity.kt
@@ -1,39 +1,48 @@
 package com.hubklo212.sunsetled
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.expandHorizontally
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.hubklo212.sunsetled.data.RepositoryImpl
+import com.hubklo212.sunsetled.presentation.SunsetViewModel
 import com.hubklo212.sunsetled.ui.theme.SunsetLEDTheme
+import kotlinx.coroutines.flow.collectLatest
 
 class MainActivity : ComponentActivity() {
+
+    private val mainVm by viewModels<SunsetViewModel>(factoryProducer = {
+        object: ViewModelProvider.Factory{
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return SunsetViewModel(RepositoryImpl(RetrofitInstance.api))
+                    as T
+            }
+        }
+    })
+
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         setContent {
             SunsetLEDTheme {
                 // A surface container using the 'background' color from the theme
@@ -41,69 +50,47 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Instrukcja()
-                    UpdateButton()
+                    //Instrukcja()
+                    mainVm.UpdateButton {
+                        LocationUtils.fetchLocation(this, fusedLocationProviderClient) { latitude, longitude ->
+                            Toast.makeText(
+                                applicationContext,
+                                "$latitude $longitude",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            mainVm.updateSunsetTime(latitude, longitude)
+                        }
 
+                    }
+                    val sunsetTime = mainVm.sunset.collectAsState().value
+                    val context = LocalContext.current
+                    
+                    LaunchedEffect(key1 = mainVm.showErrorToastChannel){
+                        mainVm.showErrorToastChannel.collectLatest { show ->
+                            if (show) {
+                                Toast.makeText(
+                                    context, "Error",Toast.LENGTH_SHORT
+                                ).show()
+                            }
+
+                        }
+                    }
+                    if (sunsetTime.results.sunset.isBlank()) {
+                        Row(modifier = Modifier.fillMaxSize(),
+                            content ={
+                                CircularProgressIndicator()
+                            })
+                    } else {
+                        Row(modifier = Modifier
+                            .padding(8.dp)
+                            .fillMaxSize(),
+                            horizontalArrangement = Arrangement.Center){
+                            Text(text = sunsetTime.results.sunset)
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Instrukcja() {
-    Row(
-        verticalAlignment = Alignment.Top,
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        Text( " Witaj w aplikacji przeznaczonej do konfiguracji lampki! " +
-                "Oto, krótka instrukcja co i jak:\n" +
-                "1. Przycisk \"Update manually\" wysyła do lampki dzisiejszą godzinę zachodu słońca\n" +
-                "2. Pole Auto-update włącza automatyczne pobieranie przez lampkę godziny zachodu\n" +
-                "3.")
-    }
-}
-
-@Composable
-fun UpdateButton() {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Button(
-            onClick = { /*TODO*/ },
-            modifier = Modifier
-        ) {
-            Text("Update manually")
-        }
-        Spacer (modifier = Modifier.height(24.dp))
-        Cosiek()
-    }
-}
-
-@Composable
-fun Cosiek() {
-    var isChecked by remember { mutableStateOf(false) }
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        Checkbox(
-            checked = isChecked,
-            onCheckedChange = { isChecked = it }
-        )
-        Text("Auto-update")
-    }
-}
-
-
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun GreetingPreview() {
-    SunsetLEDTheme {
-        Instrukcja()
-        UpdateButton()
-    }
-}
